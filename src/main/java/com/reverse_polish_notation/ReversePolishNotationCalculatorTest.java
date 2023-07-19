4f7bbda345fd88e4da15f137741580148ddb6e40
Andrey_Tar <unknownmanty@yandex.ru>
2023-07-19 18:46:44 +0300

    first

---
+++ src/main/java/com/reverse_polish_notation/ReversePolishNotationCalculatorTest.java
@@ -0,0 +1,10 @@
+package com.reverse_polish_notation;
+import org.junit.Test;
+
+public class ReversePolishNotationCalculatorTest {
+
+    @Test
+    public void shouldCalculateAddition() {
+
+    }
+}
