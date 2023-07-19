b6adbd2fe19297c8a9f129197285ecb6ca55a857
Andrey_Tar <unknownmanty@yandex.ru>
2023-07-19 19:08:10 +0300

    100% coverage

--- src/main/java/com/reverse_polish_notation/ReversePolishNotationCalculatorTest.java
+++ src/main/java/com/reverse_polish_notation/ReversePolishNotationCalculatorTest.java
@@ -1,10 +1,51 @@
 package com.reverse_polish_notation;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 
 public class ReversePolishNotationCalculatorTest {
 
     @Test
     public void shouldCalculateAddition() {
+        ReversePolishNotationCalculator calculator = new ReversePolishNotationCalculator();
+        String addition = "4 3 +";
+        assertEquals(7, calculator.calculatePolishNotation(addition));
+    }
 
+    @Test
+    public void shouldCalculateMultiplication() {
+        ReversePolishNotationCalculator calculator = new ReversePolishNotationCalculator();
+        String addition = "4 3 *";
+        assertEquals(12, calculator.calculatePolishNotation(addition));
+    }
+
+    @Test
+    public void shouldCalculateAdditionWithSpaces() {
+        ReversePolishNotationCalculator calculator = new ReversePolishNotationCalculator();
+        String addition = "   4       3       +      ";
+        assertEquals(7, calculator.calculatePolishNotation(addition));
+    }
+
+    @Test
+    public void shouldCalculateMultiplicationWithSpaces() {
+        ReversePolishNotationCalculator calculator = new ReversePolishNotationCalculator();
+        String addition = "       4        3       *        ";
+        assertEquals(12, calculator.calculatePolishNotation(addition));
+    }
+
+    @Test
+    public void shouldCalculateSubtraction() {
+        ReversePolishNotationCalculator calculator = new ReversePolishNotationCalculator();
+        String addition = "4 3 -";
+        assertEquals(1, calculator.calculatePolishNotation(addition));
+    }
+
+    @Test
+    public void shouldCalculateSubtractionWithSpaces() {
+        ReversePolishNotationCalculator calculator = new ReversePolishNotationCalculator();
+        String addition = "       4        3       -        ";
+        assertEquals(1, calculator.calculatePolishNotation(addition));
     }
 }
